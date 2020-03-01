3d281a8488fae270d879d464a8106787bf288e04
gogochen <1097968473@qq.com>
2020-03-01 23:22:27 +0800

    first commit

---
+++ app/controller/Demo.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm
+ * author  :gogochen
+ * date    :2020/3/1
+ * time    :0:05
+ */
+
+namespace app\controller;
+
+
+use app\BaseController;
+
+class Demo extends BaseController
+{
+    public function show()
+    {
+        return "abba";
+    }
+
+}
